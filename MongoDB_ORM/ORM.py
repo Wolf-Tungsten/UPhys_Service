014371e0148bb0cbb01bba7ccaf286dbafd15b77
Wolf Tungsten <gaoruihao@outlook.com>
2017-10-14 01:11:38 +0800

    补充了ORM定义及说明细节

--- MongoDB_ORM/ORM.py
+++ MongoDB_ORM/ORM.py
@@ -1,8 +1,13 @@
 from .collections.category import Category
-
+from .collections.user import User
+from .collections.question import Question
+from .collections.answer import Answer
 
 class ORM(object):
     def __init__(self, db):
         self.db = db
 
         self.category = Category(self.db)
+        self.user = User(self.db)
+        self.question = Question(self.db)
+        self.answer = Answer(self.db)
