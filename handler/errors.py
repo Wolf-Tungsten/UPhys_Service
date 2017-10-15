a5db45c58f957d488b73b266feb4ece5a20e808d
fanyc <fancy9801@gmail.com>
2017-10-15 21:20:49 +0800

    错误处理

---
+++ handler/errors.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+__author__ = 'panhui'
+
+RES_NOT_EXIST = 100
+
+
+PERMISSIONS_DENIED = 101
+
+
+STATE_ERROR = 102
+
+
+MISSING_ARGS = 103
+
+
+ARGS_ERROR = 104
+
+RELATE_RES_ERROR = 105
+
+FREQUENCY_ERROR = 106
