ea41f6212d073bfe606a0fe3f7213cd3fc151299
fanyc <fancy9801@gmail.com>
2017-10-27 14:57:24 +0800

    修改文档

--- handler/user.py
+++ handler/user.py
@@ -60,7 +60,9 @@ class UserHandler(BaseHandler):
     {
         "status": "success",
         "code": "200",
-        "result": "ok"
+        "result": {
+            "token": [身份令牌]
+            }
     }
     
     @apiError Error 401 AuthError 身份认证失败或获取用户名失败
