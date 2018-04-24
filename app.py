e461c4b4feadba65ca6c085d14e8c3fb5535be74
WolfTungsten <canis.tungsten@foxmail.com>
2018-04-24 20:10:06 +0800

    添加pm2配置文件，修改默认端口号

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from MongoDB_ORM.ORM import ORM
 import routes
 
 
-define("port", default=7942, help="本地监听端口", type=int)
+define("port", default=7901, help="本地监听端口", type=int)
 define("DEBUG", default=True, help="是否开启debug模式", type=bool)
 define("TEST", default=True, help="测试服务器，支持跨域访问,推送测试模式", type=bool)
 define("db_name", default="tank", help="mongodb数据库名称", type=str)
